9ab357bad28a945d56dfbaa82801d9f995a9ad70
Tomas Martinez <tomasurielmp@gmail.com>
2023-05-12 16:38:14 -0300

    Skeleton Animatons and Behaviour

--- Assets/Scripts/EnemiesLogic/Projectile.cs
+++ Assets/Scripts/EnemiesLogic/Projectile.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace EnemiesLogic
 {
     public class Projectile : MonoBehaviour
-    {
-        private Rigidbody _rb;
-
-
-        private void Awake()
-        {
-            _rb = GetComponentInChildren<Rigidbody>();
-        }
-
-        private void Update()
-        {
-            _rb.velocity = Vector3.right * 10;
-        }
-
+    { 
         private void OnCollisionEnter(Collision other)
         {
             Destroy(gameObject);
--- Assets/Scripts/EnemiesLogic/SkeletonController.cs
+++ Assets/Scripts/EnemiesLogic/SkeletonController.cs
@@ -10,26 +10,31 @@ namespace EnemiesLogic
         private NavMeshAgent _nma;
         private GameObject _player;
 
-        public float timeBetweenShots = 1f;
+        private Animator _anim;
+
+        public float timeBetweenShots = 3f;
         private float _shotTimer;
         private void Awake()
         {
             _nma = GetComponent<NavMeshAgent>();
             _player = GameObject.Find("Player");
             _shotTimer = timeBetweenShots;
+            _anim = GetComponentInChildren<Animator>();
         }
 
         void ShootArrow()
         {
-            Quaternion shootingAngle = Quaternion.Euler(_player.transform.position - shootingPoint.position);
-            Instantiate(arrowPrefab, shootingPoint.position, Quaternion.Euler(new Vector3(shootingAngle.x, 0, -90)));
+            GameObject arrow = Instantiate(arrowPrefab, shootingPoint.position, transform.rotation);
+            Rigidbody arrowRb = arrow.GetComponent<Rigidbody>();
+            _anim.SetTrigger("Shoot");
+            arrowRb.AddForce(shootingPoint.forward * 5f, ForceMode.Impulse);
         }
 
         private void Update()
         {
+            HandleAnimation();
             _nma.SetDestination(_player.transform.position);
-
-            if (!_nma.pathPending && _shotTimer <= 0)
+            if (_nma.velocity == Vector3.zero && _shotTimer <= 0)
             {
                 ShootArrow();
                 _shotTimer = timeBetweenShots;
@@ -38,6 +43,11 @@ namespace EnemiesLogic
             {
                 _shotTimer -= Time.deltaTime;
             }
+        }
+
+        void HandleAnimation()
+        {
+            _anim.SetFloat("Velocity", _nma.velocity.magnitude);
         }
     }
 }
